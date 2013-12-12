afcb8984ab45eb020f20e5ecbd9140560c5b169b
zomerfeld <zomerfeld@gmail.com>
2013-12-12 11:08:04 -0800

    Header fixes, style changes

--- page.php
+++ page.php
@@ -20,14 +20,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-						<div class="entry-thumbnail">
-							<?php the_post_thumbnail(); ?>
-						</div>
-						<?php endif; ?>
 
-					</header><!-- .entry-header -->
 
 					<div class="entry-content">
 						<?php the_content(); ?>
